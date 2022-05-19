44e8a9d5ea6052abb5db0d5e9000c8e0fa32cfff
AliSakanak <102181822+AliSakanak@users.noreply.github.com>
2022-05-19 16:40:31 +0100

    Week 5 implemented, Now onto implementing week 6 changes. MiniProject-Offline has been created to allow for usage without connection to a database (i.e. data stored locally in .dat files)

--- MiniProject.py
+++ MiniProject.py
@@ -1,18 +1,19 @@
+from logging import exception
 import pickle
+import pymysql
+from time import sleep
 
 try:
-    products = pickle.load(open("products.dat", "rb"))
-    print("\nProducts save found and loaded.")
-except FileNotFoundError:
-    print("\nExisting products not found. Creating products file.")
-    products = []
-
-try:
-    couriers = pickle.load(open("couriers.dat", "rb"))
-    print("\nCouriers save found and loaded.")
-except FileNotFoundError:
-    print("\nExisting couriers not found. Creating couriers file.")
-    couriers = []
+    connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="kelebek18",
+    db="mini_project"
+    )
+    print ("Connected to cafe's database")
+except pymysql.OperationalError as e:
+    print(f"Error: {e}\nUnable to successfully connect to Database.")
+#sleep(1)
 
 try:
     orders = pickle.load(open("orders.dat", "rb"))
@@ -82,39 +83,207 @@ def index_list(item_list):
         print(f"[{i}] {item}")
 
 
+def view_list_new(item_list):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {item_list}")
+    result = cursor.fetchall()
+    column_names = [i[0] for i in cursor.description]
+    column_names_string = ", ".join(column_names).title()
+    if len(result) == 0:
+        print(f"{item_list} table is empty".capitalize())
+    else:
+        print(f"\n({column_names_string})")
+        for item in result:
+            print(item)
+        print(f"Number of items: {len(result)}")
+    cursor.close()
+
 def view_list(item_list):
     print(item_list)
     print(f"Number of items: {len(item_list)}")
 
-
-def add_new_item(item_list):
-    item_name = input("Type the item you want to add: ").title().strip()
-    item_list.append(item_name)
-    print(f"{item_name} added to list.")
-
-
-def update_item(item_list):
+def add_new_product():
     try:
-        user_choice = int(input("Type number of item you wish to update: "))
-        old_item = item_list[user_choice]
-        new_item = input("Type name of new item: ").title().strip()
-        item_list[user_choice] = new_item
-        print(f"{old_item} changed to {new_item}.")
-    except IndexError:
-        print("No such number item exists.")
+        cursor = connection.cursor()
+        product_name = input("Type the name of product to add: ").title().strip()
+        product_price = float(input("Type the price of the product: "))
+        sql = f"INSERT INTO products (name, price) VALUES (%s,%s)"
+        cursor.execute(sql, (product_name, product_price))
+        print(f"\n{product_name} successfully added to products.")
+        connection.commit()
+        cursor.close()
     except ValueError:
-        print("Please enter the number of the item, not the word.")
+        print(f"Please input the correct value type for the associated field.")
 
-
-def delete_item(item_list):
+def add_new_courier():
     try:
-        deleted_item = int(input("Type number of item you wish to delete: "))
-        print(f"Deleted {item_list[deleted_item]} from list.")
-        del item_list[deleted_item]
-    except IndexError:
-        print("No such number item exists.")
+        cursor = connection.cursor()
+        courier_name = input("Type the name of courier to add: ").title().strip()
+        courier_phone = input("Type the phone number of the courier: ").strip()
+        sql = f"INSERT INTO couriers (name, phone_number) VALUES (%s,%s)"
+        cursor.execute(sql, (courier_name, courier_phone))
+        print(f"\n{courier_name} successfully added")
+        connection.commit()
+        cursor.close()
     except ValueError:
-        print("Please enter the number of the item, not the word.")
+        print(f"Please input the correct value type for the associated field.")
+
+def add_new_order():
+    try:
+        cursor = connection.cursor()
+        customer_name = input("Type customer name: ").title().strip()
+        customer_address = input("Type customer address: ").title().strip()
+        customer_phone_number = input("Type customer's phone number: ").strip()
+        if len(customer_name) == 0 or len(customer_address) == 0 or len(customer_phone_number) == 0:
+            print(f"Error: Input cannot be blank.")
+            return
+        view_list_new("products")
+        products_input = input("Type ID of products you wish to order, seperated by commas: ")
+        removed_spaces = products_input.replace(" ","")
+        listed_version = removed_spaces.split(",")
+        products_list_chosen = []
+        for number in listed_version:
+            cursor.execute(f"SELECT * FROM mini_project.products WHERE products_id = {number}")
+            products_choice = cursor.fetchone()
+            products_list_chosen.append(products_choice)
+        print(f"*{products_list_chosen} added to order*")
+        view_list_new("couriers")
+        courier_input = int(input("Type ID of courier you wish to use: "))
+        cursor.execute(f"SELECT couriers_id FROM mini_project.couriers WHERE couriers_id = {courier_input}")
+        courier_choice = cursor.fetchone()[0]
+        print(f"*{courier_choice} chosen as courier*\n")
+        sql = f"""INSERT INTO mini_project.orders (customer_name, customer_address, customer_phone, couriers_id, status_id, products_id) 
+        VALUES (%s,%s,%s,%s,%s,%s)"""
+        cursor.execute(sql, (customer_name, customer_address, customer_phone_number, courier_input, 1, products_input))
+        connection.commit()
+        cursor.close()
+        print("Order successfully created!")
+    except ValueError:
+        print("Error: Please enter correct number associated with courier.")
+    except IndexError:
+        print("Error: No such number item exists.")
+    except TypeError:
+        print("Error: Values entered cannot be blank unless stated otherwise.")
+
+
+def update_courier():
+    try:
+        cursor = connection.cursor()
+        user_choice = int(input("Type the courier_id of courier you wish to update: "))
+        cursor.execute(f"SELECT * FROM couriers WHERE couriers_id = {user_choice}")
+        old_item = cursor.fetchone()
+        print(old_item)
+        if old_item == None:
+            print(f"ERROR: The couriers_id you have entered ({user_choice}) could not be found")
+            return
+        print(f"*{old_item} selected*")
+        new_name = input("Type updated name of courier, or leave blank for no change: ").strip().title()
+        new_number = input("Type the courier phone number, or leave blank for no change: ").strip()
+        if len(new_name) > 0:
+            cursor = connection.cursor()
+            sql = F"UPDATE couriers SET name = \'{new_name}\' WHERE couriers_id = {user_choice}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print(f"\n*{old_item} updated to {new_name} successfully*")
+        else:
+            print('\n*Product name not changed as entry was left blank*')
+        if len(new_number) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE couriers SET phone_number = {new_number} WHERE couriers_id = {user_choice}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print(f"*{old_item} updated to {new_number} successfully*")
+        else:
+            print('*Courier phone number not changed as entry was left blank*')
+    except ValueError:
+        print(f"ERROR: Please input the correct value type for the associated field")
+    except pymysql.OperationalError:
+        print(f"ERROR: operationalerror")
+
+def update_product():
+    try:
+        cursor = connection.cursor()
+        user_choice = int(input("Type the product_id of item you wish to update: "))
+        cursor.execute(f"SELECT * FROM products WHERE products_id = {user_choice}")
+        old_item = cursor.fetchone()
+        if old_item == None:
+            print(f"ERROR: The products_id you have entered ({user_choice}) could not be found")
+            return
+        print(f"*{old_item} selected*")
+        new_name = input("Type updated name of product, or leave blank for no change: ").strip().title()
+        new_price = input("Type the updated product price, or leave blank for no change: ").strip()
+        if len(new_name) > 0:
+            cursor = connection.cursor()
+            sql = F"UPDATE products SET name = \'{new_name}\' WHERE products_id = {user_choice}"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print(f"\n*{old_item} updated to {new_name} successfully*")
+        else:
+            print('\n*Product name not changed as entry was left blank*')
+        if len(new_price) > 0:
+            cursor = connection.cursor()
+            sql = f"UPDATE products SET price = {new_price} WHERE (products_id = {user_choice})"
+            cursor.execute(sql)
+            connection.commit()
+            cursor.close()
+            print(f"*{old_item} updated to {new_price} successfully*")
+        else:
+            print('*Product price not changed as entry was left blank*')
+    except ValueError:
+        print(f"ERROR: Please input the correct value type for the associated field")
+    except pymysql.OperationalError:
+        print(f"ERROR: Please ensure that price is a float value")
+
+
+#def add_new_item(item_list):
+    # item_name = input("Type the item you want to add: ").title().strip()
+    # item_list.append(item_name)
+    # print(f"{item_name} added to list.")
+    # #INSERT INTO `mini_project`.`products` (`product_id`, `name`, `price`) VALUES ('2', 'Coke', '0.99');
+
+
+# def update_item(item_list):
+#     try:
+#         user_choice = int(input("Type number of item you wish to update: "))
+#         old_item = item_list[user_choice]
+#         new_item = input("Type name of new item: ").title().strip()
+#         item_list[user_choice] = new_item
+#         print(f"{old_item} changed to {new_item}.")
+#     except IndexError:
+#         print("No such number item exists.")
+#     except ValueError:
+#         print("Please enter the number of the item, not the word.")
+
+def delete_item_new(table):
+    try:
+        cursor = connection.cursor()
+        deleted_item = int(input("Type ID number of item you wish to delete: "))
+        cursor.execute(f"SELECT * FROM {table} WHERE {table}_id = {deleted_item}")
+        selection = cursor.fetchone()
+        if selection == None:
+            print(f"\nError: Item associated with chosen ID ({deleted_item}) not found.")
+            return
+        cursor.execute(f"DELETE FROM {table} WHERE {table}_id = {deleted_item}")
+        connection.commit()
+        cursor.close()
+        print(f"\n*Deleted {selection} from database*")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+
+
+# def delete_item(item_list):
+#     try:
+#         deleted_item = int(input("Type number of item you wish to delete: "))
+#         print(f"Deleted {item_list[deleted_item]} from list.")
+#         del item_list[deleted_item]
+#     except IndexError:
+#         print("No such number item exists.")
+#     except ValueError:
+#         print("Please enter the number of the item, not the word.")
 
 
 def create_new_order():
@@ -130,7 +299,8 @@ def create_new_order():
             "Customer_Address": customer_address,
             "Customer_Phone_Number": customer_phone_number,
             "Courier": courier_choice,
-            "Status": order_status_list[0],
+            "Status": order_status_list[0]
+            #"Ordered Items": [indexes from productlist] 
         }
         orders.append(order_dictionary)
         print("Order successfully created!")
@@ -149,6 +319,15 @@ order_status_list = [
     "nearby",
     "delivered",
 ]
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="kelebek18",
+    db="mini_project"
+)
+
+
 new_launch = True
 while new_launch:
     print(
@@ -156,6 +335,7 @@ while new_launch:
         " the number seen in [square brackets] and pressing Enter."
     )
     new_launch = False
+    #sleep(3)
 while True:
     try:
         main_menu()
@@ -181,15 +361,15 @@ while True:
         if product_menu_choice == 0:
             break
         elif product_menu_choice == 1:
-            view_list(products)
+            view_list_new("products")
         elif product_menu_choice == 2:
-            add_new_item(products)
+            add_new_product()
         elif product_menu_choice == 3:
-            index_list(products)
-            update_item(products)
+            view_list_new("products")
+            update_product()
         elif product_menu_choice == 4:
-            index_list(products)
-            delete_item(products)
+            view_list_new("products")
+            delete_item_new("products")
         else:
             print("Enter a valid option")
     while option == 2:
@@ -202,15 +382,15 @@ while True:
         if courier_menu_choice == 0:
             break
         elif courier_menu_choice == 1:
-            view_list(couriers)
+            view_list_new("couriers")
         elif courier_menu_choice == 2:
-            add_new_item(couriers)
+            add_new_courier()
         elif courier_menu_choice == 3:
-            index_list(couriers)
-            update_item(couriers)
+            view_list_new("couriers")
+            update_courier()
         elif courier_menu_choice == 4:
-            index_list(couriers)
-            delete_item(couriers)
+            view_list_new("couriers")
+            delete_item_new("couriers")
         else:
             print(
                 "Invalid option. Please choose a correct number from the list for the corresponding action."
@@ -225,9 +405,9 @@ while True:
         if orders_menu_choice == 0:
             break
         elif orders_menu_choice == 1:
-            view_list(orders)
+            view_list_new("orders")
         elif orders_menu_choice == 2:
-            create_new_order()
+            add_new_order()
         elif orders_menu_choice == 3:
             index_list(orders)
             try:
